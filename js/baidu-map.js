ce9ef5e18c7aff4abfc7bccb41f2816b6d5bfacb
lilong <601721519@qq.com>
2015-08-10 08:34:00 +0800

    underscore.js

--- js/baidu-map.js
+++ js/baidu-map.js
@@ -4,49 +4,7 @@ require.config({
 	}
 });
 
-define(['jquery','backbone'], function($){
-	"use strict";
-	
-	widgetMap = Backbone.View.extend({
-		
-		initialize:function(options) {
-			var obj = this;
-			
-			obj._initBDMapCallBack();
-			obj._loadBDMap();
-		},
-		
-		/**
-		 * @description 异步载入百度地图
-		 */
-		 _loadBDMap: function() {
-			 var obj = this
-			     , script = document.createElement("script");
-			 script.type = "text/javascript";
-			 script.src = "http://api.map.baidu.com/api?v=2.0&ak=您的密钥&callback=MapCallback";
-			 document.body.appendChild(script);
-		},
-		
-		/**
-		 * @description 设置地图回调函数
-		 */
-		_initBDMapCallBack: function() {
-			var obj = this;
-
-			window.MapCallback = function() {
-				// 借鉴单例模式
-				obj.map = new BMap.Map("allmap");
-				obj.point = new BMap.Point(116.404, 39.915);
-
-				obj.map.centerAndZoom(obj.point,15);
-				obj.map.enableScrollWheelZoom();
-			}
-		}
-	});
-	return widgetMap;
-});
-
-require(['widgets/map'], function(widgetMap){
+require(['map'], function(widgetMap){
 	new widgetMap({
 		el: "#xxx"
 	});
---
+++ js/map.js
@@ -0,0 +1,40 @@
+define(['jquery','backbone'], function($){
+	
+	widgetMap = Backbone.View.extend({
+		
+		initialize:function(options) {
+			var obj = this;
+			
+			obj._initBDMapCallBack();
+			obj._loadBDMap();
+		},
+		
+		/**
+		 * @description 异步载入百度地图
+		 */
+		 _loadBDMap: function() {
+			 var obj = this;
+			 var script = document.createElement("script");
+			 script.type = "text/javascript";
+			 script.src = "http://api.map.baidu.com/api?v=1.4&ak=ACe388fab11b70137379e717f23663f7&callback=MapCallback";
+			 document.body.appendChild(script);
+		},
+		
+		/**
+		 * @description 设置地图回调函数
+		 */
+		_initBDMapCallBack: function() {
+			var obj = this;
+
+			window.MapCallback = function() {
+				// 借鉴单例模式
+				obj.map = new BMap.Map("allmap");
+				obj.point = new BMap.Point(116.404, 39.915);
+
+				obj.map.centerAndZoom(obj.point, 15);
+				obj.map.enableScrollWheelZoom();
+			}
+		}
+	});
+	return widgetMap;
+});
